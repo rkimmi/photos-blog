edbb37c31da2f1f9afe87c779b574cd90373b1f9
Kimmi Rindel <kimmirindel@Kimmis-MacBook-Pro.local>
2024-10-13 14:22:18 +1300

    Add changing bg-color

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-#### Description
+### Description
 
-Serves github pages for rkimmi.github.io /photos-blog.
+Serves UI for rkimmi.github.io/photos-blog. Uses Svelte + TS + Vite and gh-pages.
 
-#### Getting started
+### Getting started
+
+run `npm install` then `npm run dev` to start the app.
+
+#### Deploy github pages
+
+run `npm run build` to outout build files to /dist
+
+run `npm run deploy` to deploy the app to rkimmi.github.io/photos-blog. NOTE: does not push the latest to git.
+
+
---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+
+  addEventListener("DOMContentLoaded", (event) => {
+    const body = document.getElementsByTagName('body')[0];
+    const [backgroundColor, fontColor] = getColorsForHour();
+
+    body.style.backgroundColor = backgroundColor;
+    body.style.color = fontColor;
+});
+
+const hoursToColourMap: Map<number, string[]> = new Map([
+    [0, ['#bd1818', 'white']], // maroon red bg, white font
+    [12, ['#F9EDCD', 'brown']], // eggshell bg, brown font
+    [16, ['#18BD70', 'black']], // slime green bg, black font
+    [20, ['#A7A7F3', 'white']] // light blue bg, white font
+])
+
+function getColorsForHour(): [string, string] {
+    let selectedBackgroundColor = '';
+    let selectFontColor = '';
+    const currentDateTime = new Date();
+    const hours = currentDateTime.getHours();
+
+    for (let [key, [backgroundColor, fontColor]] of hoursToColourMap) {
+        if (hours >= key) { 
+            selectedBackgroundColor = backgroundColor;
+            selectFontColor = fontColor
+        } 
+        else {
+            break;
+        }
+    }
+
+    return [selectedBackgroundColor, selectFontColor];
+}
+</script>
+
+<main>
+  <div class="card">
+    wip
+    <!-- <Counter /> -->
+  </div>
+</main>
+
+<style>
+    main {
+      width: 100vw;
+      height: 100vh;
+    }
+</style>
